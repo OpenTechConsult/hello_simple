8e594a73df0fd2c2b78baeae041ffd1a94e0d118
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-01-04 17:33:14 +0000

    Create a minimalist Express application without express-generator

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+let app = express();
+
+app.all('*', (req, res) => {
+    res.send('Welcome to Practical Node.js!');
+})
+
+app.listen(3000, () => {
+    console.log('Open at localhost:3000');
+})
